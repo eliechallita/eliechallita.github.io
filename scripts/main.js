3f27eeef1b29c9db240bfc20db9e7fecd63a22a7
eliechallita <eliechallita76@gmail.com>
2024-05-23 11:49:05 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/chromeicon.jpg") {
+    myImage.setAttribute("src", "images/firefox2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/chromeicon.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
